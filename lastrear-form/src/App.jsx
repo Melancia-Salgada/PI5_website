6f5b3513ac4a1b06d3b3356ba86aa1b1ba6e327d
turtoga <gabrielalmeida.dev@outlook.com>
2025-04-24 10:41:03 -0300

    init

---
+++ lastrear-form/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+
+  return (
+   <>
+    <div className='content-container'>
+      aaaa
+    </div>
+   </>
+  )
+}
+
+export default App
