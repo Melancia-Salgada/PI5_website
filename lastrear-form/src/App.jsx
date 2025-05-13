71c9a30bc4f1b3f265ea5367673d460cfb03e6e0
Gabriel Almeida <GabrielAlmeida.dev@outlook.com>
2025-05-12 23:36:28 -0300

    feat: variavel para participante 2

--- lastrear-form/src/App.jsx
+++ lastrear-form/src/App.jsx
@@ -1,17 +1,34 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import './App.scss'
 import Header from './components/Header'
 import Form from './components/Form'
+import Title from './components/Title'
 
 function App() {
+
+  const [segundoParticipante, degundoParticipante] = useState(false)
 
   return (
    <>
     <main className='content-container'>
       <Header/>
       <div className='engloba'>
+        {segundoParticipante && 
+          <>
+            <Title nome={'Participante 1'}/>
+            <Form/>
+          </>
+        }
+
         <Form/>
+        {segundoParticipante && 
+          <>
+            <Title nome={'Participante 2'}/>
+            <Form/>
+          </>
+        }
       </div>
+      
     </main>
    </>
   )
